b42329a606e700247f6d4add356dc16240fb99ec
vadexito <david.mizrahi@m4x.org>
2012-12-09 10:31:56 +0100

    nav bar right desktop version

--- application/modules/events/views/helpers/ListCategories.php
+++ application/modules/events/views/helpers/ListCategories.php
@@ -1,18 +1,22 @@
 <?php
 
-class Layout_View_Helper_ListCategories extends Zend_View_Helper_Abstract
+class Events_View_Helper_ListCategories extends Zend_View_Helper_Abstract
 {
     
     public function listCategories($partialView,$namePage)
     {   
-        $mainMenu = new Zend_Navigation();
-        $this->view->navigation()->menu()->setPartial($partialView);
-        
         if ($this->view->navigation()->findById($namePage))
         {
+            $this->view->navigation()->menu()->setPartial($partialView);
+            $mainMenu = new Zend_Navigation();
             $mainMenu->addPage($this->view->navigation()->findById($namePage));
+            return $this->view->navigation()->menu($mainMenu);
         }
-        return $this->view->navigation()->menu($mainMenu);
+        else
+        {
+            return '';
+        }
+        
     }
     
     static public function compEvents($a,$b)
