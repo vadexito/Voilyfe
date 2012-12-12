9dfaa86afdc776adcc0014fc95156e8e7127cfad
vadexito <david.mizrahi@m4x.org>
2012-12-12 09:40:36 +0100

    button event desktop version fix

--- application/modules/events/views/helpers/ButtonNewEvent.php
+++ application/modules/events/views/helpers/ButtonNewEvent.php
@@ -3,20 +3,28 @@
 class Events_View_Helper_ButtonNewEvent extends Zend_View_Helper_Abstract
 {
     
-    public function buttonNewEvent($categoryId)
+    public function buttonNewEvent()
     {
-        if ($categoryId)
+        $view = $this->view;
+        $wrap = ['tag' => 'div','class' => 'row-fluid'];
+        
+        // add button for right page
+        
+        if (!property_exists($view,'form') && (!$view->allOption) &&
+            (property_exists($view,'category') && is_object($view->category->categories) && $view->category->categories->count()=== 0))
         {
-            return '<a id="new-event-btn" href ="'
-            .$this->view->url(
-                    array(
-                        'action' => 'create',
-                        'containerId'=> $categoryId),
-                    'event'
+            
+            return '<'.$wrap['tag'].' class="'.$wrap['class'].'">
+                <a id="new-event-btn" href ="'
+            .$view->url(
+                ['action' => 'create','containerId'=> $view->category->id],
+                'event'
             )
             .'" class="pull-right btn btn-primary btn-large">'."\n"
-            .'<i class="icon-edit icon-white"></i> '.$this->view->translate('menu_createNewEvent')
-            .'</a>';
+            .'<i class="icon-edit icon-white"></i> '
+            .$view->translate('menu_createNewEvent')
+            .'</a>
+                </div>';
         }
         return '';
     }
