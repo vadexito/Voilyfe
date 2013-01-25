12e79aff53477758280835415d0a825227386a29
vadexito <david.mizrahi@m4x.org>
2013-01-25 23:28:02 +0100

    enhance design add event category menu

--- application/modules/events/views/helpers/CategoryListLine.php
+++ application/modules/events/views/helpers/CategoryListLine.php
@@ -3,7 +3,8 @@
 class Events_View_Helper_CategoryListLine extends Zend_View_Helper_HtmlElement
 {
     
-    public function categoryListLine($page,$imgPath,$bubbleCount=true,$lineSplit=false)
+    public function categoryListLine($page,$imgPath,$bubbleCount=true,
+            $lineSplit=false,$icon = 'plus')
     {
         $categoryName = $page->getLabel();
         $category = preg_replace('#category_#','',$categoryName);
@@ -14,22 +15,36 @@ class Events_View_Helper_CategoryListLine extends Zend_View_Helper_HtmlElement
         $nbEvents = count($page->events);
         $href = $page->getHref();
         
+        $options=[
+            'href'              => $href,
+            'imgSrc'            => $imgSrc,
+            'categoryName'      => $categoryName,
+            'nbEvents'          => $nbEvents,
+            'bubbleCount'       => $bubbleCount,
+            'lineSplit'         => $lineSplit,
+            'categoryId'        => $page->categoryId,
+            'icon'              => $icon        
+        ];
+        
+        
+        
+        
         return array(
-            'render' => $this->_render($href,$imgSrc,$categoryName,$nbEvents,$bubbleCount,$lineSplit,$page->categoryId),
+            'render' => $this->_render($options),
             'countEvent' => $nbEvents
         );
     }
     
-    protected function _render($href,$imgSrc,$categoryName,
-            $nbEvents,$bubbleCount,$lineSplit,$categoryId)
+    protected function _render($options)
     {
+        extract($options); //options: href, imgSrc, categoryName, nbEvents, bubbleCount, lineSplit, categoryId, icon
         $hrefCreate = $this->view->url(array('action' => 'create','containerId' => $categoryId),'event');
         if (($nbEvents == 0) && $lineSplit)
         {
             $href = $hrefCreate;
         }
         
-        return "\t".'<li>
+        return "\t".'<li data-icon="'.$icon.'">
             <a href="'.$href.'" data-ajax="false">
                 <img src="'. $imgSrc .'"/>
                     <h3>'.$this->view->translate($categoryName). '</h3>'."\n"
