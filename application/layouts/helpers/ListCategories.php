23b50bbbe55906006dc6511dd5bca0b64e967fc5
vadexito <david.mizrahi@m4x.org>
2013-01-25 19:48:51 +0100

    improve helper list categories

--- application/layouts/helpers/ListCategories.php
+++ application/layouts/helpers/ListCategories.php
@@ -2,25 +2,44 @@
 
 class Application_View_Helper_ListCategories extends Zend_View_Helper_Abstract
 {
+    //comparison function for ordering the categories in function of the number 
+    // of events in the category
+    public $compEvents = NULL;
     
-    public function listCategories($partialView,$namePage)
+    public function listCategories($partialView = NULL,$namePage = NULL)
     {   
-        if ($this->view->navigation()->findById($namePage))
+        if ($partialView & $namePage)
         {
-            $this->view->navigation()->menu()->setPartial($partialView);
-            $mainMenu = new Zend_Navigation();
-            $mainMenu->addPage($this->view->navigation()->findById($namePage));
-            return $this->view->navigation()->menu($mainMenu);
+            if ($this->view->navigation()->findById($namePage))
+            {
+                $this->view->navigation()->menu()->setPartial($partialView);
+                $mainMenu = new Zend_Navigation();
+                $mainMenu->addPage($this->view->navigation()->findById($namePage));
+                return $this->view->navigation()->menu($mainMenu);
+            }
+            else
+            {
+                return '';
+            }
         }
         else
         {
-            return '';
+           $this->initCompEvents();
+           return $this;
         }
         
     }
     
-    static public function compEvents($a,$b)
+    public function initCompEvents()
     {
-        return ($b['countEvent'] - $a['countEvent']);
+        if (!$this->compEvents)
+        {
+            $this->compEvents = function ($a,$b)
+            {
+                return ($b['countEvent'] - $a['countEvent']);
+            };
+        }
     }
+    
+    
 }
